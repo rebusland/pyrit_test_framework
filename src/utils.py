dcb1feb23d1916caf3d95421a60e386e15051ea3
andrea.R <andrea.rebolini@hotmail.com>
2025-08-27 12:31:05 +0200

    Added utility for counting message tokens using tiktoken library

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import time
 from functools import wraps
 import logging
 from typing import Any, Callable
+import tiktoken
 
 from logging_handler import logger
 
@@ -55,3 +56,13 @@ def log_execution_time(return_time: bool = False, level: int = logging.INFO):
                 return (result, elapsed) if return_time else result
             return wrapper
     return decorator
+
+def num_tokens_for_model(text: str, model_name: str) -> int:
+    # GPT-4o should use o200k_base encoding
+    try:
+        model_name = model_name or 'cl100k_base' # fallback encoding if nothing provided
+        enc = tiktoken.encoding_for_model(model_name=model_name)
+    except KeyError:
+        logger.warning(f"Cannot find enconding for given model {model_name}, falling back to a default encoding")
+        enc = tiktoken.get_encoding("cl100k_base")  # fallback encoding
+    return len(enc.encode(text))
